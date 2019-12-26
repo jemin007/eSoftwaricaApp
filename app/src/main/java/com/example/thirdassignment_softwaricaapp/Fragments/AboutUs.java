09ad7ead888a1370d9d9846b2a0040c38c3ae850
jemin <jeminshrestha9@gmail.com>
2019-12-26 22:02:07 +0545

    about us done with errors

--- app/src/main/java/com/example/thirdassignment_softwaricaapp/Fragments/AboutUs.java
+++ app/src/main/java/com/example/thirdassignment_softwaricaapp/Fragments/AboutUs.java
@@ -1,6 +1,8 @@
 package com.example.thirdassignment_softwaricaapp.Fragments;
 
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +10,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.thirdassignment_softwaricaapp.R;
 
@@ -16,6 +20,7 @@ import com.example.thirdassignment_softwaricaapp.R;
  */
 public class AboutUs extends Fragment {
 
+    WebView webView;
 
     public AboutUs() {
         // Required empty public constructor
@@ -26,7 +31,14 @@ public class AboutUs extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+        View view= inflater.inflate(R.layout.fragment_about_us, container, false);
+
+        webView= view.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com/");
+        return view;
     }
 
 }
