79bcfddd5683c4274e250b1dc0e0a300cbd4bbf0
jemin <jeminshrestha9@gmail.com>
2019-12-26 21:20:20 +0545

    fixed errors

--- app/src/main/java/com/example/thirdassignment_softwaricaapp/Fragments/ShowStudent.java
+++ app/src/main/java/com/example/thirdassignment_softwaricaapp/Fragments/ShowStudent.java
@@ -4,13 +4,18 @@ package com.example.thirdassignment_softwaricaapp.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.thirdassignment_softwaricaapp.Adapter.StudentsAdapter;
+import com.example.thirdassignment_softwaricaapp.Dashboard;
 import com.example.thirdassignment_softwaricaapp.R;
 
 /**
@@ -18,7 +23,7 @@ import com.example.thirdassignment_softwaricaapp.R;
  */
 public class ShowStudent extends Fragment {
 
-
+    RecyclerView recyclerView;
     public ShowStudent() {
 
     }
@@ -28,7 +33,18 @@ public class ShowStudent extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show_student, container, false);
+        View view = inflater.inflate(R.layout.fragment_show_student, container, false);
+        recyclerView = view.findViewById(R.id.tvRecycleShow);
+        if (Dashboard.studentsList.isEmpty()) {
+            Toast.makeText(getContext(), "Empty", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            StudentsAdapter studentsAdapter = new StudentsAdapter(getContext(), Dashboard.studentsList);
+            recyclerView.setAdapter(studentsAdapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        }
+
+        return view;
     }
 
 }
